f3b11ab5475c0fccfc06d3efadbe7b8186e43726
Moustafa Ascoura <moustafa.a@blackrockts.com>
2024-08-23 06:19:49 +0300

    Static pages routing

--- EveAccessories/accessories/urls.py
+++ EveAccessories/accessories/urls.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import index
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('categories', categories, name='categories'),
+    path('products', products, name='products'),
+    path('product/<int:id>', product, name='product'),
+    path('cart', cart, name='cart'),
+    path('about', about, name='about')
 ]
--- EveAccessories/accessories/views.py
+++ EveAccessories/accessories/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def categories(request):
+    return render(request, 'categories.html')
+
+
+def products(request):
+    return render(request, 'products.html')
+
+
+def product(request, id):
+    return render(request, 'product.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
+
+
+def about(request):
+    return render(request, 'about.html')
