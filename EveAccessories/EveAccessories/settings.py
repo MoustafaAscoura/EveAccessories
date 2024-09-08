38339d7733315ceb6f398169e076c4d808dbec2f
Moustafa Ascoura <moustafa.a@blackrockts.com>
2024-09-08 20:45:33 +0300

    static configs

--- EveAccessories/EveAccessories/settings.py
+++ EveAccessories/EveAccessories/settings.py
@@ -110,13 +110,7 @@ APPEND_SLASH = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = ""
-
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join("static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
