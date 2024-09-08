97f6cfd1bf80a231a3bbe2f83483537d9d7bc0c9
Moustafa Ascoura <moustafa.a@blackrockts.com>
2024-09-08 18:17:33 +0300

    config

--- EveAccessories/EveAccessories/settings.py
+++ EveAccessories/EveAccessories/settings.py
@@ -7,16 +7,10 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') in ['TRUE', '1']
-
-ALLOWED_HOSTS = []
+DEBUG = os.getenv('DEBUG', "False") in ['TRUE', '1']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
